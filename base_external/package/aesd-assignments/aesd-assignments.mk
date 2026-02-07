30494c7006dd609299035e527788e2c0cfb60bdf
tps <tps@Ubuntu.myguest.virtualbox.org>
2026-02-07 16:15:03 -0500

    assignment-4-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4ea0704154abc33a9a6d4bfa1c6c60a1a0c418ed
+AESD_ASSIGNMENTS_VERSION = 47a6e122dec715febc5499929c12824b0ae3a623
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
